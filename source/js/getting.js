0a905f240298758ef52a23cb4bfe5cb2d7cc2112
看客之眼 <361876798whyr@sina.com>
2023-10-16 01:53:25 +0800

    v1.0.0

--- source/js/getting.js
+++ source/js/getting.js
@@ -8,6 +8,24 @@ gettext = () => {
 	}).catch(() => { })
 }
 
+getcomment = () => {
+	const file = new Request("https://tenapi.cn/v2/comment")
+	fetch(file, {
+		method: 'GET'
+	}).then((res) => res.json()).then((data) => {
+		let info = document.querySelector("#music-info"),
+			comment = document.querySelector("#music-comment"),
+			by = document.querySelector("#comment-by"),
+			datachild = data.data
+
+		info.innerHTML = `<div class="cover"><img src="${datachild.cover}"><audio id="songs" src="https://api.injahow.cn/meting/?type=url&id=${datachild.id}"></audio><div id="play-button" class="music-button"><i class="fa fa-play-circle-o"></i></div><div id="pause-button" class="music-button" style="display:none;"><i class="fa fa-pause-circle-o"></i></div></div><div class="info"><p>${datachild.songs}</p><p>${datachild.sings}</p></div>`
+		comment.innerHTML = datachild.comment
+		by.innerHTML = 'by - ' + datachild.name
+		playaudio()
+		pauseaudio()
+	}).catch(() => { })
+}
+
 gettoday = () => {
 	const file = new Request("https://www.mxnzp.com/api/history/today?type=0&app_id=9ihnlkkoncgpgjqp&app_secret=HUYArsgkw8WqtfaXxcWvm6uHiTcGVg0J")
 	fetch(file, {
@@ -34,10 +52,44 @@ gettoday = () => {
 		}
 		else {
 			for (i = 0; i < 10; i++) {
-				history.innerHTML += `<ul class="histroy_list"><li>${dataList[newItem].year}年${dataList[newItem].month}月${dataList[newItem].day}日：</li><li>${dataList[newItem].title}；</li></ul>`			}
+				history.innerHTML += `<ul class="histroy_list"><li>${dataList[newItem].year}年${dataList[newItem].month}月${dataList[newItem].day}日：</li><li>${dataList[newItem].title}；</li></ul>`
+			}
 		}
 	}).catch(() => { })
 }
 
+reflash = () => {
+	let reflash = document.getElementById('reflash')
+	reflash.addEventListener('click', () => {
+		location.reload()
+	})
+}
+
+playaudio = () => {
+	let play_button = document.getElementById('play-button'),
+		pause_button = document.getElementById('pause-button'),
+		songs = document.getElementById('songs')
+	play_button.addEventListener('click', () => {
+		songs.play()
+		play_button.style.setProperty('display', 'none')
+		pause_button.style.setProperty('display', 'block')
+		console.log('play')
+	})
+}
+
+pauseaudio = () => {
+	let play_button = document.getElementById('play-button'),
+		pause_button = document.getElementById('pause-button'),
+		songs = document.getElementById('songs')
+	pause_button.addEventListener('click', () => {
+		songs.pause()
+		play_button.style.setProperty('display', 'block')
+		pause_button.style.setProperty('display', 'none')
+		console.log('pause')
+	})
+}
+
 gettext()
+getcomment()
 gettoday()
+reflash()
